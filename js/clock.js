56694ebad6204d3d6d5eca797d638457cc59de23
huoty <sudohuoty@163.com>
2015-05-17 22:54:06 +0800

    05172254

--- js/clock.js
+++ js/clock.js
@@ -9,13 +9,13 @@ function LoadBlogParts(){
 	var sUrl = swfUrl;
 	
 	var sHtml = "";
-	sHtml += '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="140" height="80" id="' + swfTitle + '" align="middle">';
+	sHtml += '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="220" height="100" id="' + swfTitle + '" align="middle">';
 	sHtml += '<param name="allowScriptAccess" value="always" />';
 	sHtml += '<param name="movie" value="' + sUrl + '" />';
 	sHtml += '<param name="quality" value="high" />';
 	sHtml += '<param name="bgcolor" value="#ffffff" />';
 	sHtml += '<param name="wmode" value="transparent" />';
-	sHtml += '<embed wmode="transparent" src="' + sUrl + '" quality="high" bgcolor="#ffffff" width="140" height="80" name="' + swfTitle + '" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />';
+	sHtml += '<embed wmode="transparent" src="' + sUrl + '" quality="high" bgcolor="#ffffff" width="220" height="110" name="' + swfTitle + '" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />';
 	sHtml += '</object>';
 	
 	document.write(sHtml);
